77cb6d408ec58a1b028635ac54fba41dee416fcd
KJ201112 <kingjamesgado100@gmail.com>
2026-03-21 11:48:55 +0800

    Refactor applyToChildren to applyOpacityToChildren

--- src/main.cpp
+++ src/main.cpp
@@ -5,16 +5,19 @@ using namespace geode::prelude;
 
 static bool g_invisibleMode = false;
 
-static void applyToChildren(CCNode* node, bool invisible) {
+static void applyOpacityToChildren(CCNode* node, GLubyte opacity) {
     if (!node) return;
 
     for (auto child : CCArrayExt<CCNode*>(node->getChildren())) {
         if (typeinfo_cast<PlayerObject*>(child)) {
-            child->setVisible(true);
-            continue;
+            continue; // players are handled separately
         }
-        child->setVisible(!invisible);
-        applyToChildren(child, invisible);
+
+        if (auto rgba = dynamic_cast<CCRGBAProtocol*>(child)) {
+            rgba->setOpacity(opacity);
+        }
+
+        applyOpacityToChildren(child, opacity);
     }
 }
 
@@ -34,20 +37,21 @@ class $modify(InvisibleObjectsMod, PlayLayer) {
         m_fields->buttonLabel = label;
 
         auto bg = CCScale9Sprite::create("square02_small.png");
-        bg->setContentSize({ 80.f, 24.f });
+        bg->setContentSize({80.f, 24.f});
         bg->setOpacity(180);
 
+        // Fixed create call (explicit nullptr for selected sprite - more reliable)
         auto item = CCMenuItemSpriteExtra::create(
-            bg, this,
+            bg, nullptr,
+            this,
             menu_selector(InvisibleObjectsMod::onToggleButton)
         );
 
-        label->setPosition({ 40.f, 12.f });
+        label->setPosition({40.f, 12.f});
         bg->addChild(label);
 
-        auto menu = CCMenu::create();
-        menu->addChild(item);
-        menu->setPosition({ 50.f, winSize.height - 25.f });
+        auto menu = CCMenu::createWithItem(item);
+        menu->setPosition({50.f, winSize.height - 25.f});
         this->addChild(menu, 9999);
 
         return true;
@@ -56,17 +60,17 @@ class $modify(InvisibleObjectsMod, PlayLayer) {
     void applyInvisible(bool invisible) {
         if (!m_objectLayer) return;
 
-        applyToChildren(m_objectLayer, invisible);
+        GLubyte op = invisible ? 0 : 255;
+        applyOpacityToChildren(m_objectLayer, op);
 
-        // Always force both players visible no matter what
+        // Force players + their children fully visible
         if (m_player1) {
-            m_player1->setVisible(true);
-            // Also show all children of player1
-            applyToChildren(m_player1, false);
+            m_player1->setOpacity(255);
+            applyOpacityToChildren(m_player1, 255);
         }
         if (m_player2) {
-            m_player2->setVisible(true);
-            applyToChildren(m_player2, false);
+            m_player2->setOpacity(255);
+            applyOpacityToChildren(m_player2, 255);
         }
     }
 
@@ -76,32 +80,28 @@ class $modify(InvisibleObjectsMod, PlayLayer) {
         applyInvisible(g_invisibleMode);
 
         if (m_fields->buttonLabel) {
-            m_fields->buttonLabel->setString(
-                g_invisibleMode ? "EYE:ON " : "EYE:OFF"
-            );
+            m_fields->buttonLabel->setString(g_invisibleMode ? "EYE:ON" : "EYE:OFF");
         }
 
         if (g_invisibleMode) {
-            Notification::create(
-                "Invisible Mode ON - Good luck!",
-                NotificationIcon::Warning
-            )->show();
+            Notification::create("Invisible Mode ON - Good luck!", NotificationIcon::Warning)->show();
         } else {
-            Notification::create(
-                "Invisible Mode OFF - Objects visible",
-                NotificationIcon::Success
-            )->show();
+            Notification::create("Invisible Mode OFF - Objects visible", NotificationIcon::Success)->show();
         }
-
-        log::info("Invisible mode toggled");
     }
 
     void addObject(GameObject* obj) {
         PlayLayer::addObject(obj);
+
         if (g_invisibleMode && obj) {
             if (!typeinfo_cast<PlayerObject*>(obj)) {
-                obj->setVisible(false);
-                applyToChildren(obj, true);
+                if (auto rgba = dynamic_cast<CCRGBAProtocol*>(obj)) {
+                    rgba->setOpacity(0);
+                }
+                applyOpacityToChildren(obj, 0);
+            } else {
+                obj->setOpacity(255);
+                applyOpacityToChildren(obj, 255);
             }
         }
     }
@@ -120,5 +120,5 @@ class $modify(InvisibleObjectsMod, PlayLayer) {
 };
 
 $on_mod(Loaded) {
-    log::info("Level Invisible Mod loaded");
+    log::info("Invisible Objects Mod loaded");
 }
